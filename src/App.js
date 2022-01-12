3bce52cbea19b88e940b5b1e90456a1e572238a4
unknown <deekaynine@gmail.com>
2022-01-12 18:59:24 -0500

    added prototype of store page and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
