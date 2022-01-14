0bed92bce2f6ad5bba0fa7e81661ccb11fcb4385
unknown <deekaynine@gmail.com>
2022-01-14 04:31:53 -0500

    added sign-in components and styles

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.jsx";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
+          <Route path="/signin" element={<SignInAndSignUpPage />} />
         </Routes>
       </BrowserRouter>
     </div>
